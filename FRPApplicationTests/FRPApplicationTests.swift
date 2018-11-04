1b3490ce164a29255bbbf2a3b7615a02c57f86d0
Karim Ebrahem <karimabdelazizmansour@gmail.com>
2018-11-04 23:12:12 +0200

    Initial Commit

---
+++ FRPApplicationTests/FRPApplicationTests.swift
@@ -0,0 +1,34 @@
+//
+//  FRPApplicationTests.swift
+//  FRPApplicationTests
+//
+//  Created by Karim Ebrahem on 11/4/18.
+//  Copyright © 2018 KarimEbrahem. All rights reserved.
+//
+
+import XCTest
+@testable import FRPApplication
+
+class FRPApplicationTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
